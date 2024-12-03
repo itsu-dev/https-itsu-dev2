310a8dad997ff9dd00dd25ccde074c2dbf994b75
itsu-dev <itsu020402@gmail.com>
2024-12-03 19:18:25 +0900

    feat: 自己紹介の実装

--- src/features/components/index/TopSection/index.tsx
+++ src/features/components/index/TopSection/index.tsx
@@ -1,11 +1,18 @@
 import FullPageSection from "../../../../components/FullPageSection";
 import styles from "./styles.module.css";
 import RecentImages from "./RecentImages";
+import Introduction from "./Introduction";
+import React from "react";
 
 export default function TopSection() {
+    const ref = React.useRef<HTMLTableSectionElement>(null);
     return (
         <FullPageSection as="div" className={styles.wrapper}>
             <RecentImages />
+            <section ref={ref} className={styles.introduction}>
+                <h2 className="hidden">自己紹介</h2>
+                <Introduction />
+            </section>
         </FullPageSection>
     );
 }
---
+++ src/hooks/useObserver.ts
@@ -0,0 +1,27 @@
+import {RefObject, useEffect, useRef} from "react";
+
+export default function useObserver(
+  callback: IntersectionObserverCallback,
+  target: RefObject<HTMLElement | null>,
+  options?: IntersectionObserverInit,
+) {
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver((entry, observer) => {
+      const e = entry.find(e => e.target === target.current);
+      if (e && e.isIntersecting) {
+        callback(entry, observer);
+        observer.disconnect();
+      }
+    }, options);
+    if (target.current) {
+      observer.current?.observe(target.current);
+    }
+    return () => {
+      observer.current?.disconnect();
+    };
+  }, []);
+
+  return observer;
+}
